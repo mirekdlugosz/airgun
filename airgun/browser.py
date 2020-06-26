55326736964040c5ec1519e704ad790a4a536590
Mirek Długosz <mzalewsk@redhat.com>
2020-06-26 18:50:12 +0200

    Fix downloaded files support
    Since Chrome 80, downloads.Manager is private and we can't access it
    from page script anymore.
    
    Code provided by Itay Levy on https://stackoverflow.com/a/60429467/3552063

--- airgun/browser.py
+++ airgun/browser.py
@@ -658,13 +658,12 @@ class AirgunBrowser(Browser):
         downloads_uri = 'chrome://downloads'
         if not self.url.startswith(downloads_uri):
             self.url = downloads_uri
-        return self.execute_script(
-            """
-            return downloads.Manager.get().items_
-              .filter(e => e.state === "COMPLETE")
-              .map(e => e.file_url || e.fileUrl);
-        """
-        )
+        return self.execute_script("""
+            return document.querySelector('downloads-manager')
+              .shadowRoot.querySelector('#downloadsList')
+              .items.filter(e => e.state === 'COMPLETE')
+              .map(e => e.filePath || e.file_path || e.fileUrl || e.file_url);
+        """)
 
     def get_file_content(self, uri):
         """Get file content by its URI from browser's downloads page.
